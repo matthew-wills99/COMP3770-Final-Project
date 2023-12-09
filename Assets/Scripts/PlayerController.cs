88f6d7ad95f216c6d43e200d1090d12becff490b
Matthew Wills <wills5@uwindsor.ca>
2023-12-09 15:48:48 -0500

    Player Movement & Blink Ability

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,72 @@
+using System;
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speedMultiplier = 0.5f; // Adjust the speed of the player
-    float xDir;
-    float zDir;
-    void FixedUpdate()
+
+    [Header("References")]
+    public Transform orientation;
+    public Rigidbody rb;
+    public TrailRenderer tr;
+
+    [Header("Movement")]
+    public float speedMultiplier = 25f; // Adjust the speed of the player, 25 works well
+
+    [Header("Blink")]
+    public float blinkDistance = 5f; // Adjust how far the player blinks
+    public float blinkCooldown = 5f; // Adjust the cooldown of the blink ability in seconds
+    private bool canBlink = true;
+
+    private Vector3 dir; // Store the movement direction of the player
+    
+    void Start()
     {
-        xDir = Input.GetAxis("Horizontal");
-        zDir = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(xDir, 0f, zDir) * speedMultiplier;
-        transform.position += moveDir;
+        dir = new Vector3();
+        dir = Vector3.zero;
+
+        orientation.transform.position = new Vector3(0, 1, blinkDistance);
     }
-}
+
+    void Update()
+    {
+        Move();
+
+        // Player blinks
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canBlink)
+        {
+            StartCoroutine(Blink());
+        }
+    }
+
+    public IEnumerator Blink()
+    {
+        Debug.Log("Blink");
+        canBlink = false;
+        tr.emitting = true;
+
+        transform.position = orientation.position;
+
+        yield return new WaitForSeconds(0.1f);
+        tr.emitting = false;
+        yield return new WaitForSeconds(blinkCooldown);
+
+        canBlink = true;
+        Debug.Log("Blink ready.");
+    }
+
+    void Move()
+    {
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+        dir.Normalize();
+
+        transform.position += speedMultiplier * Time.deltaTime * dir;
+    
+        if(dir != Vector3.zero)
+        {
+            transform.forward = dir;
+        }
+    }
+}   
